d7e056ab7be6df1bd423e08f903e115cf23865b6
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-03-14 18:10:24 +0000

    override create function

--- users/serializers.py
+++ users/serializers.py
@@ -9,6 +9,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['email', 'user_name', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 class UsersListSerializer(serializers.ModelSerializer):
     class Meta:
