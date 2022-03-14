d43dcf218122f6ddea858a1a200f44dcdaaa1b9e
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-03-14 18:38:58 +0000

    create logout url path

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns =[
     path('', views.UsersList.as_view(), name='users_list'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/blacklist/', views.BlacklistTokenUpdateView.as_view(), name='blacklist'),
 ]
