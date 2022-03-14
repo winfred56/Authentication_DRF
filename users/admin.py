8d073d4f756dfc548d495abb8c353b5c48f11e46
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-03-14 17:07:02 +0000

    edit admin site view

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,32 @@ from django.contrib import admin
 from .models import UserManger
 # Register your models here.
 
+
+
+
+class UserAdminConfig(UserAdmin):
+    model = NewUser
+    search_fields = ('email', 'username', 'first_name',)
+    list_filter = ('email', 'username', 'first_name', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'username', 'first_name',
+                    'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'first_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Personal', {'fields': ('about',)}),
+    )
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
+
 admin.site.register(UserManger)
